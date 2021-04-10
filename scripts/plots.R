6bb6bfabefff11b04b401f8ea67198b194497a35
Felipe Figueiredo <philsf79@gmail.com>
2021-04-09 22:41:51 -0300

    plots setup standardized

--- scripts/plots.R
+++ scripts/plots.R
@@ -2,70 +2,78 @@
 
 library(survminer)
 
+xlabel <- "Time (months)"
+pval.coord <- c(150, 0.10)
+
 # main figure -------------------------------------------------------------
 
 gg.overal <- ggsurvplot(
   survfit(se ~ 1, dtmin),
   dtmin
-  )
+  ) +
+  ggtitle(
+    label = "Overall survival PJI confirmed death after TJA",
+    subtitle = "Considering PJI lab-confirmed deaths"
+    ) +
+  xlab(xlabel)
 
 # additional figures ------------------------------------------------------
 
 gg.sex <- ggsurvplot(
   survfit(se ~ sex, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by sex") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.sirs <- ggsurvplot(
   survfit(se ~ sirs, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by Confirmed SIRS") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.comp <- ggsurvplot(
   survfit(se ~ comp, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival given complications") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.joint <- ggsurvplot(
   survfit(se ~ joint, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by operated joint") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.stat.tja <- ggsurvplot(
   survfit(se ~ stat.tja, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by success of TJA") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.outcome <- ggsurvplot(
   survfit(se ~ outcome, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by surgery outcome") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 gg.comp.type <- ggsurvplot(
   survfit(se ~ comp.type, dtmin),
   dtmin,
-  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  pval = TRUE, pval.coord = pval.coord, pval.size = pval.size
   ) +
   ggtitle("Survival by type of complication") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
 # boxplot(dtmin$time, ylab = "Time (years)", main = "Distribution of follow-up time in the cohort")
 
@@ -78,5 +86,5 @@ gg.cr <- ggcompetingrisks(
   multiple_panels = FALSE
   ) +
   ggtitle("Competing risks for causes of death after TJA") +
-  xlab("Time (months)")
+  xlab(xlabel)
 
