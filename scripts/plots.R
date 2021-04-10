720bce5ab944770476cc801ba8202d5e9b3cd6e5
Felipe Figueiredo <philsf79@gmail.com>
2021-04-09 21:48:06 -0300

    plots code indented

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,21 +1,104 @@
 library(survminer)
 
-ggsurvplot(survfit(se ~ 1, dtmin), dtmin)
-ggsave("figures/overall.png")
-ggsurvplot(survfit(se ~ sex, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by sex") + xlab("Time (months)")
-ggsave("figures/sex.png")
-ggsurvplot(survfit(se ~ sirs, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by Confirmed SIRS") + xlab("Time (months)")
-ggsave("figures/sirs.png")
-ggsurvplot(survfit(se ~ comp, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival given complications") + xlab("Time (months)")
-ggsave("figures/complication.png")
-ggsurvplot(survfit(se ~ joint, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by operated joint") + xlab("Time (months)")
-ggsave("figures/joint.png")
-ggsurvplot(survfit(se ~ stat.tja, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by success of TJA") + xlab("Time (months)")
-ggsave("figures/status.png")
-ggsurvplot(survfit(se ~ outcome, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by surgery outcome") + xlab("Time (months)")
-ggsave("figures/result.png")
-ggsurvplot(survfit(se ~ comp.type, dtmin), dtmin, pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size) + ggtitle("Survival by type of complication") + xlab("Time (months)")
-ggsave("figures/type.png")
+gg.overal <- ggsurvplot(
+  survfit(se ~ 1, dtmin),
+  dtmin
+  )
+ggsave(filename = "figures/overall.png",
+       plot = print(gg.overal),
+       width = 7, height = 7
+       )
+
+gg.sex <- ggsurvplot(
+  survfit(se ~ sex, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by sex") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/sex.png",
+  plot = print(gg.sex),
+  width = 7, height = 7
+  )
+
+gg.sirs <- ggsurvplot(
+  survfit(se ~ sirs, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by Confirmed SIRS") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/sirs.png",
+  device = print(gg.sirs),
+  width = 7, height = 7
+  )
+
+gg.comp <- ggsurvplot(
+  survfit(se ~ comp, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival given complications") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/complication.png",
+  plot = print(gg.comp),
+  width = 7, height = 7
+  )
+
+gg.joint <- ggsurvplot(
+  survfit(se ~ joint, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by operated joint") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/joint.png",
+  plot = print(gg.joint),
+  width = 7, height = 7
+  )
+
+gg.stat.tja <- ggsurvplot(
+  survfit(se ~ stat.tja, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by success of TJA") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/status.png",
+  plot = print(gg.stat.tja),
+  width = 7, height = 7
+  )
+
+gg.outcome <- ggsurvplot(
+  survfit(se ~ outcome, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by surgery outcome") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/result.png",
+  plot = print(gg.outcome),
+  width = 7, height = 7
+  )
+
+gg.comp.type <- ggsurvplot(
+  survfit(se ~ comp.type, dtmin),
+  dtmin,
+  pval = TRUE, pval.coord = c(150, 0.10), pval.size = pval.size
+  ) +
+  ggtitle("Survival by type of complication") +
+  xlab("Time (months)")
+ggsave(
+  filename = "figures/type.png",
+  plot = print(gg.comp.type),
+  width = 7, height = 7
+  )
 
 # boxplot(dtmin$time, ylab = "Time (years)", main = "Distribution of follow-up time in the cohort")
 
