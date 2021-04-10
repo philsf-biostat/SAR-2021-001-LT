74ebc3f5cf78b65728c7703320c81af246214dfd
Felipe Figueiredo <philsf79@gmail.com>
2021-04-09 22:33:09 -0300

    gg plots included in results script (for the report)

--- scripts/results.R
+++ scripts/results.R
@@ -2,3 +2,4 @@ source('scripts/input.R', encoding = 'UTF-8')
 
 source("scripts/kaplan-meier.R", encoding = 'UTF-8')
 source("scripts/descriptive.R", encoding = 'UTF-8')
+source("scripts/plots.R", encoding = 'UTF-8')
